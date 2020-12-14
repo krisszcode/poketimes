15489f07707d8a86db34557a4747e2f2bd171151
krisszcode <nagy.ace.krisztian@gmail.com>
2020-12-14 20:59:42 +0100

    get the route parameters

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Rainbow from '../hoc/Rainbow';
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consectetur perspiciatis doloremque aliquam commodi. A in, optio excepturi necessitatibus voluptates nostrum veritatis et. Nobis nemo beatae esse perferendis dolores laborum.</p>
+        </div>
+    )
+}
+
+export default Rainbow(About)
---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = (props) => {
+    // setTimeout(() => {
+    //     props.history.push('/about');
+    // }, 2000);
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consectetur perspiciatis doloremque aliquam commodi. A in, optio excepturi necessitatibus voluptates nostrum veritatis et. Nobis nemo beatae esse perferendis dolores laborum.</p>
+        </div>
+    )
+}
+
+export default Contact
